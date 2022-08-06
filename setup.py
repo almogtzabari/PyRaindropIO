c1f85b3f77646ed07497ab23bb8f40c9f83b50b9
Almog Tzabari <37792505+almogtzabari@users.noreply.github.com>
2022-08-06 21:50:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
       name='pyraindropio',
       version='0.1',
-      description='Python wrapper for Raindrop.io',
+      description='Python wrapper for Raindrop.io API',
       author='Almog Tzabari',
       author_email='https://github.com/almogtzabari/PyRaindropIO/',
       url='https://www.python.org/sigs/distutils-sig/',
