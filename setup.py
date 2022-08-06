2bf77bbb3b7e909f09e124dd79b4947d15c32189
almogtzabari <almogtzabari@campus.technion.ac.il>
2022-08-06 21:49:49 +0300

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+      name='pyraindropio',
+      version='0.1',
+      description='Python wrapper for Raindrop.io',
+      author='Almog Tzabari',
+      author_email='https://github.com/almogtzabari/PyRaindropIO/',
+      url='https://www.python.org/sigs/distutils-sig/',
+      install_requires=['requests'],
+     )
